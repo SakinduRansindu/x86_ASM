7615d2031370e9862111ef62f58b8d568b893d42
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2024-12-30 22:01:45 +0530

    read multy digit number

---
+++ readOnePlusDigits.asm
@@ -0,0 +1,61 @@
+section .data
+    input_buffer db 8                ; Buffer for input (size 10 bytes)
+    integer_result dd 0               ; To store the converted integer
+    new_line db 0xA                   ; Newline character
+    reader_pointer db 0
+
+section .bss
+    buffer resb 8                    ; Allocate space for the input
+
+section .text
+    global _start
+
+_start:
+    ; Prompt for input
+    mov eax, 4
+    mov ebx, 1
+    lea ecx, [input_buffer]
+    mov edx, 14
+    int 0x80
+
+    ; Read input
+    mov eax, 3
+    mov ebx, 0
+    lea ecx, [buffer]
+    mov edx, 10                       ; Read up to 10 characters
+    int 0x80
+
+    ; Convert input to integer
+    xor eax, eax                      ; Clear result
+    xor ebx, ebx                      ; Index for processing input
+    mov ecx, [buffer]
+
+convert_loop:
+    sub cl, '0' 
+
+    cmp cl, 0                        ; Check if character is less than '0'
+    jl done_conversion
+    cmp cl, 9                        ; Check if character is greater than '9'
+    jg done_conversion
+
+
+    xor eax, eax
+    mov al , byte [integer_result]         ; Load current integer result
+    mov bl , 10
+    mul bl
+    add al, cl                      ; Add new digit value
+    mov [integer_result], al         ; Store updated result
+
+    xor eax, eax
+    mov al, byte [reader_pointer]
+    inc al
+    mov [reader_pointer], al
+    mov ecx, [buffer+eax]
+
+    jmp convert_loop
+
+
+done_conversion:
+    mov eax, 1
+    lea ebx, [integer_result]
+    int 0x80
